5efea2d6c281c7bcede843707445c602ca007e14
tarekhassoun <th36@hw.ac.uk>
2021-05-28 01:05:25 +0100

    deleting old files

--- constants.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$site_name = "alanhar";
-$page_title = ucwords($site_name);
-
-// define("API_BASE_URL", "http://dekaneh_api.tarek:8000/api/");
-// define("API_STORE_URL", API_BASE_URL . "store/");
